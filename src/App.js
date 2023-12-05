aa98f2709402a8ca746627a48738b29885f01a46
Federico-I <threxos.48@gmail.com>
2023-12-05 23:30:29 +0000

    react-state-logic

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
+import { useState } from "react";
+
 const messages = ["Learn React", "Apply for Jobs", "Invest your New Income"];
 
 export default function App() {
 
-  const step = 1;
+  const [step, setStep ] = useState (1);
+  const [nameTest, setNameTest] =useState();
 
   function handlePrevious() {
-    alert("Previous");
+    if (step > 1) setStep(step - 1);
+    setStep(step - 1);
   };
 
   function handleNext() {
-    alert("Next");
+    if (step < 3) setStep(step + 1);
+    //setStep(step + 1);
+
+    setNameTest({name: "Fred"});
   };
 
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 1 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
-      <p className="message">Step {step}: {messages[step-1]}</p>
+      <p className="message">Step {step}: {messages[step-1]} {nameTest}</p>
 
       <div className="buttons">
         <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handlePrevious}>Previous</button>
