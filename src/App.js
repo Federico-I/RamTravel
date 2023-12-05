db02e035bad568a956b50ef5a1173d3a0048dcd4
Federico-I <threxos.48@gmail.com>
2023-12-05 23:15:28 +0000

    variables-set-up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+const messages = ["Learn React", "Apply for Jobs", "Invest your New Income"];
 
-import './App.css';
+export default function App() {
 
-function App() {
+  const step = 1;
+
+  function handlePrevious() {
+    alert("Previous");
+  };
+
+  function handleNext() {
+    alert("Next");
+  };
+
+
   return (
-    
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">Step {step}: {messages[step-1]}</p>
+
+      <div className="buttons">
+        <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handlePrevious}>Previous</button>
+        <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handleNext}>Next</button>
+      </div>
+    </div>
   );
 }
 
-export default App;
