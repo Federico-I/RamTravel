a4e05fe57b50aaebae07fbb0e08e113fa7d353ad
Federico-I <threxos.48@gmail.com>
2023-12-11 10:07:07 +0000

    itme-rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return(
-    <div>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
@@ -25,9 +30,21 @@ function Form() {
 
 function PackingList() {
   return(
-    <div className="list">Ready List</div>
+    <div className="list">
+      <ul>
+        {initialItems.map((item)=> (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
   )
 };
+
+function Item({ item }) {
+  return <li>{item.description}</li>
+}
+
+
 
 function Stats() {
   return(
