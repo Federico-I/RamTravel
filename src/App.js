d5378ced4c48707c4733bff269d48ee6435d5c89
Federico-I <threxos.48@gmail.com>
2023-12-13 11:04:21 +0000

    Controlled-element

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -22,21 +24,28 @@ function Logo() {
 
 function Form() {
 
+  // Controlled Element
+
+  const [itemName, setItemName ] = useState("");
+  const [amount, setAmount] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!itemName) return;
   }
   
   return(
     <form className="add-form" onSubmit={handleSubmit}>
-      {/* Two different ways to prevent  page from deafult reload on submittion. 
+      {/* Two different ways to prevent the page from deafult reload on submittion. 
         1) - By handling event on a funtion as is shown above or inline
         2) - Inline - Ex: <form className="add-form" onSubmit={(e)=> handleSubmit(e)}
       */}
       <h3>What do you need for your trip</h3>
-      <select>
+      <select value={amount} onChange={(e)=> setAmount(Number(e.target.value ))}>
         {Array.from({ lenght: 20 }, (_, i) => i + 1).map((num)=> <option value={num} key={num}>{num}</option>)}
       </select>
-      <input type="type" placeholder="Item..." />
+      <input type="type" placeholder="Item..." value={itemName} onChange={(e)=> setItemName(e.target.value)}/>
       <button>Add</button>
     </form>
   )
