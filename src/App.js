386fd2b436fcf31e65f2d5facdf6cd20ce932477
Federico-I <threxos.48@gmail.com>
2023-12-19 12:07:47 +0000

    status-percentage-optionalRendering

--- src/App.js
+++ src/App.js
@@ -97,12 +97,21 @@ function Item({ item, deleteItem, checkedItem }) {
 };
 
 function Stats({ itemsAmount }) {
+  if (!itemsAmount.lenght) {
+    return (
+      <p className="footer">
+        <em>No items packed yet, the sooner you start the fastes you'll finish! arw you ready for the adventure?</em>
+      </p>
+    )
+  }
+
   const itemAmount = itemsAmount.lenght;
   const itemsPacked = itemsAmount.filter((item) => item.packed).lenght;
+  const percentagepacked = Math.round(itemsPacked / itemsAmount * 100);
 
   return(
     <footer>
-      <em>You have {itemAmount} items on your list, and you already packed {itemsPacked} (x%)</em>
+      <em>{percentagepacked === 100 ? "You got everything! Ready to go" : ` You have ${itemAmount} items on your list, and you already packed ${itemsPacked} (${percentagepacked} %) `} </em>
     </footer>
   )
 };
