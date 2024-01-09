f9d80f6952360f7a1ff7dc58025ffd6992bd1f71
Federico-I <threxos.48@gmail.com>
2024-01-09 22:54:24 +0000

    cleaning-code

--- src/Form.js
+++ src/Form.js
@@ -20,9 +20,9 @@ export default function Form({ addItems }) {
     
     return(
       <form className="add-form" onSubmit={handleSubmit}>
-        {/* Two different ways to prevent the page from deafult reload on submittion. 
-          1) - By handling event on a funtion as is shown above or inline
-          2) - Inline - Ex: <form className="add-form" onSubmit={(e)=> handleSubmit(e)}
+        {/* Two different ways to prevent the page from default reload on submit. 
+          1) - By handling event on a funtion as it is shown above or in line.
+          2) - In line - Ex: <form className="add-form" onSubmit={(e)=> handleSubmit(e)}
         */}
         <h3>What do you need for your trip</h3>
         <select value={amount} onChange={(e)=> setAmount(Number(e.target.value ))}>
